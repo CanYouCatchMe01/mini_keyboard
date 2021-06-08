ac74a70b34d56d45a84bc9fe8775819845b3354d
Marcus <17maca@stockholmscience.se>
2021-06-08 11:12:20 +0200

    digital test script

--- test_script/test_script.ino
+++ test_script/test_script.ino
@@ -1,5 +1,5 @@
-byte shifting_pins[] = {10,16,14,15,18};
-byte input_pins[] = {A6, A7, A8, A9};
+byte shifting_pins[] = {A6, A7, A8, A9};
+byte input_pins[] = {10,16,14,15,18};
 
 byte len_shifting_pins = sizeof(shifting_pins)/sizeof(shifting_pins[0]); //sizeof() = memory size
 byte len_input_pins = sizeof(input_pins)/sizeof(input_pins[0]); //sizeof() = memory size
@@ -8,17 +8,17 @@ void setup() {
   Serial.begin(9600);
   for (byte i = 0; i < len_input_pins; i++)
   {
-    pinMode(input_pins[i], INPUT);
+    pinMode(input_pins[i], INPUT_PULLUP);
   }
 
   //All shifting pins sets to input
   for (byte i = 0; i < len_shifting_pins; i++)
   {
     pinMode(shifting_pins[i], OUTPUT);
-    digitalWrite(shifting_pins[i], LOW); //Make output LOW
+    digitalWrite(shifting_pins[i], HIGH); //Make output LOW
   }
 
-  digitalWrite(shifting_pins[3], HIGH);
+  digitalWrite(shifting_pins[1], LOW);
 
 }
 
@@ -27,7 +27,7 @@ void loop() {
   {
     Serial.print(i);
     Serial.print(" ");
-    Serial.print(analogRead(input_pins[i]));
+    Serial.print(digitalRead(input_pins[i]));
     Serial.println();
   }
 
