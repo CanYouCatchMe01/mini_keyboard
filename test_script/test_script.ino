30e5c114a7599593ec1165e67e0f8373c8be904f
Marcus <17maca@stockholmscience.se>
2021-06-08 10:57:13 +0200

    working to debug the controller

--- test_script/test_script.ino
+++ test_script/test_script.ino
@@ -1,5 +1,5 @@
 byte shifting_pins[] = {10,16,14,15,18};
-byte input_pins[] = {A6, A7, A8, A9, A1, A2};
+byte input_pins[] = {A6, A7, A8, A9};
 
 byte len_shifting_pins = sizeof(shifting_pins)/sizeof(shifting_pins[0]); //sizeof() = memory size
 byte len_input_pins = sizeof(input_pins)/sizeof(input_pins[0]); //sizeof() = memory size
@@ -8,7 +8,7 @@ void setup() {
   Serial.begin(9600);
   for (byte i = 0; i < len_input_pins; i++)
   {
-    pinMode(input_pins[i], INPUT_PULLUP);
+    pinMode(input_pins[i], INPUT);
   }
 
   //All shifting pins sets to input
@@ -17,6 +17,8 @@ void setup() {
     pinMode(shifting_pins[i], OUTPUT);
     digitalWrite(shifting_pins[i], LOW); //Make output LOW
   }
+
+  digitalWrite(shifting_pins[3], HIGH);
 
 }
 
@@ -29,6 +31,6 @@ void loop() {
     Serial.println();
   }
 
-  delay(1000);
+  delay(100);
 
 }
